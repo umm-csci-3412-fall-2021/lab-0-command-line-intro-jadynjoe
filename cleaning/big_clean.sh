d0642011fa44833085d22cbd1005ca56d4762c5c
Jadyn Sondrol <sondr037@morris.umn.edu>
2021-09-02 10:31:22 -0500

    Remove the files that contain "DELETE ME!" and then make a new archive.
    Co-Authored-by: Joe Walbran <walbr037@morris.umn.edu>

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env bash
 tmp_directory_name="$(mktemp -d)"
+previous_location="$(pwd)"
 tar -x -z -f "$1" -C "$tmp_directory_name"
+cd "$tmp_directory_name" || exit
+grep -e "DELETE ME!" -r -l | while IFS='' read -r line
+do 
+	# Delete the file
+	rm "$line" 
+done
+tar -c -z -f "cleaned_$1" "$(basename "$1" .tgz)"
+mv "cleaned_$1" "$previous_location"
